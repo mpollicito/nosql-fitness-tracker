c9cb61d1dee7c4520bf5edb903b698d3e56b2b26
Matthew Pollicito <mpollicito@yahoo.com>
2021-01-21 13:50:16 -0500

    Fixed routes, added logic to handle database

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const morgan = require("morgan");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(express.static("public"));
@@ -16,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
